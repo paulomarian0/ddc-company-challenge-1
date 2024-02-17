7669eeafe77f636d74c2e8e439c32a49ae73fdd3
Paulo Mariano <paulomariano2501@gmail.com>
2024-02-17 11:13:24 -0300

    👌 IMPROVE: remove unused imports

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
-// import { appRoutes } from "./application/infra";
 import { env } from "./helpers/env";
-import { config } from "dotenv";
 import { appRoutes } from "./application/infra";
 
 const app = express();
