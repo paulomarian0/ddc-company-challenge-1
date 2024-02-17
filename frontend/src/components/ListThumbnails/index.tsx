447eff1698405b410a4d811c5a42b44cbeb4dbd9
Paulo Mariano <paulomariano2501@gmail.com>
2024-02-17 13:23:41 -0300

    📦 NEW: add text-wrap

--- frontend/src/components/ListThumbnails/index.tsx
+++ frontend/src/components/ListThumbnails/index.tsx
@@ -24,20 +24,18 @@ const ListThumbnails = () => {
 	return (
 		<div className="container mx-auto px-4 py-8">
 			<h2 className="text-2xl font-bold mb-4">Thumbnails</h2>
-			<div className="grid grid-cols-1 gap-4">
+			<div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
 				{data?.length === 0 && (
-					<p className="text-gray-500 text-center">
-						Nenhuma thumbnail encontrada
-					</p>
+					<p className="text-gray-500 text-center">Nenhum vídeo encontrado</p>
 				)}
 				{data?.map((thumbnail: IThumbnail) => (
 					<div
 						key={thumbnail.id}
-						className="bg-white rounded-lg shadow-md p-4 flex justify-between items-center"
+						className="bg-white rounded-lg shadow-md p-4 flex flex-col sm:flex-row justify-between items-center"
 					>
 						<div>
 							<h3
-								className="text-lg font-semibold mb-1 truncate"
+								className="text-lg font-semibold mb-1 truncate text-wrap"
 								title={thumbnail.name}
 							>
 								{thumbnail.name}
@@ -47,20 +45,22 @@ const ListThumbnails = () => {
 								Criado em: {new Date(thumbnail.createdAt).toLocaleString()}
 							</p>
 						</div>
-						<button
-							onClick={() => handleDownloadThumbnail(thumbnail.id)}
-							className="text-blue-500 hover:text-blue-700 focus:outline-none"
-							type="button"
-						>
-							Download
-						</button>
-						<button
-							type="button"
-							onClick={() => handleDeleteThumbnail(thumbnail.id)}
-							className="text-red-500 hover:text-red-700 focus:outline-none"
-						>
-							Deletar
-						</button>
+						<div className="mt-2 sm:mt-0 ">
+							<button
+								onClick={() => handleDownloadThumbnail(thumbnail.id)}
+								className="text-blue-500 mx-4 hover:text-blue-700 focus:outline-none mb-2 sm:mb-0 sm:mr-2 "
+								type="button"
+							>
+								Download
+							</button>
+							<button
+								type="button"
+								onClick={() => handleDeleteThumbnail(thumbnail.id)}
+								className="text-red-500 hover:text-red-700 focus:outline-none mx-4"
+							>
+								Deletar
+							</button>
+						</div>
 					</div>
 				))}
 			</div>
--- frontend/src/components/ListVideos/index.tsx
+++ frontend/src/components/ListVideos/index.tsx
@@ -24,14 +24,14 @@ const ListVideos = () => {
 	return (
 		<div className="container mx-auto px-4 py-8">
 			<h2 className="text-2xl font-bold mb-4">Vídeos</h2>
-			<div className="grid grid-cols-1  gap-4">
+			<div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
 				{data?.length === 0 && (
 					<p className="text-gray-500 text-center">Nenhum vídeo encontrado</p>
 				)}
 				{data?.map((video: IVideos) => (
 					<div
 						key={video.id}
-						className="bg-white rounded-lg shadow-md p-4 flex justify-between items-center"
+						className="bg-white rounded-lg shadow-md p-4 flex flex-col sm:flex-row justify-between items-center"
 					>
 						<div>
 							<h3
@@ -45,20 +45,22 @@ const ListVideos = () => {
 								Criado em: {new Date(video.createdAt).toLocaleString()}
 							</p>
 						</div>
-						<button
-							onClick={() => handleDownloadVideo(video.id)}
-							className="text-blue-500 hover:text-blue-700 focus:outline-none"
-							type="button"
-						>
-							Download
-						</button>
-						<button
-							type="button"
-							onClick={() => handleDeleteVideo(video.id)}
-							className="text-red-500 hover:text-red-700 focus:outline-none"
-						>
-							Deletar
-						</button>
+						<div className="mt-2 sm:mt-0 ">
+							<button
+								onClick={() => handleDownloadVideo(video.id)}
+								className="text-blue-500 mx-4 hover:text-blue-700 focus:outline-none mb-2 sm:mb-0 sm:mr-2 "
+								type="button"
+							>
+								Download
+							</button>
+							<button
+								type="button"
+								onClick={() => handleDeleteVideo(video.id)}
+								className="text-red-500 hover:text-red-700 focus:outline-none mx-4"
+							>
+								Deletar
+							</button>
+						</div>
 					</div>
 				))}
 			</div>
